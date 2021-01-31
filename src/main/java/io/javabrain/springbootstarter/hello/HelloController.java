0560e00afc0a17e0a4fee253e4adc1769a080d3f
Ashok Seervi <52868446+ashokseervi14@users.noreply.github.com>
2021-02-01 01:47:01 +0530

    Added RestController()
    added  RestController that return Object(JSON)

--- src/main/java/io/javabrain/springbootstarter/hello/HelloController.java
+++ src/main/java/io/javabrain/springbootstarter/hello/HelloController.java
@@ -1,5 +1,15 @@
 package io.javabrain.springbootstarter.hello;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
 public class HelloController {
+	
+	
+	@RequestMapping("/hello")
+	public String sayHi() {
+		return "HI";
+	}
 
 }
